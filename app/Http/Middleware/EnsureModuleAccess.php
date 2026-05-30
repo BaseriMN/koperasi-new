ac189e7a0e251bd7a1cd4828c43c6c0e09351273
BaseriMN <muhamad.baseri@gmail.com>
2026-05-30 11:17:35 +0800

    feat: takde apa sangat

--- app/Http/Middleware/EnsureModuleAccess.php
+++ app/Http/Middleware/EnsureModuleAccess.php
@@ -11,7 +11,7 @@ class EnsureModuleAccess
 {
     /**
      * Kawal akses berdasarkan matrix modul dalam DB.
-     * Penggunaan: ->middleware('module:pengurusan_ahli')
+     * Penggunaan: ->middleware('module:pengurusan_staff')
      * Super-user sentiasa dibenarkan.
      */
     public function handle(Request $request, Closure $next, string $moduleKey): Response
@@ -21,7 +21,7 @@ class EnsureModuleAccess
         if (! $user) {
             return redirect()->route('login');
         }
-
+        //dd($user->name, $moduleKey, ModuleAccess::userCan($user, $moduleKey));
         if (! ModuleAccess::userCan($user, $moduleKey)) {
             $label = config("modules.modules.{$moduleKey}.label", 'modul ini');
             abort(403, "Peranan anda tidak mempunyai akses ke {$label}.");
